4fc08ad7a5e64b7744474c5cc2d1ef52a4b26cb1
MarioCruz20 <blastedcore2025@gmail.com>
2025-11-22 18:46:16 -0600

    Procesos cartillas-recetas en navbar y router

--- view/componets/navbar.php
+++ view/componets/navbar.php
@@ -23,6 +23,18 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'home';
             <li class="nav-item <?php echo ($page === 'home') ? 'active' : ''; ?>">
                 <a class="nav-link" href="index.php?page=home">Inicio</a>
             </li>
+
+            <li class="nav-item dropdown <?php echo ($page === 'cartillas' || $page === 'recetas') ? 'active' : ''; ?>">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="procesosDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Procesos
+                </a>
+                <div class="dropdown-menu" aria-labelledby="procesosDropdown">
+                    <a class="dropdown-item" href="view/cartillaVacunacion.php">Ingreso de cartillas</a>
+                    <a class="dropdown-item" href="view/recetas.php">Ingreso de recetas</a>
+                </div>
+            </li>
+
             <?php if (isset($_SESSION['usuario']) && $_SESSION['usuario']["rol_nombre"] === "Administrador") { ?>
                 <li class="nav-item <?php echo ($page === 'usuarios') ? 'active' : ''; ?>">
                     <a class="nav-link" href="index.php?page=usuarios">Gestión de Usuarios</a>
